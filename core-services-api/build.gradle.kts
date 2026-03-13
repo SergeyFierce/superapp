7cd5e87b07dab6688f5844d5d5df0a40c65c0ac1
SergeyFierce <sergeytopskiy2003@gmail.com>
2026-03-13 15:18:27 +0300

    четвёртый коммит

--- core-services-api/build.gradle.kts
+++ core-services-api/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
-kotlin {
-    jvmToolchain(11)
-}
-
 dependencies {
     implementation(project(":core-common"))
     implementation(libs.androidx.compose.ui)
