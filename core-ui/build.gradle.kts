ea50d71376c8785e91cc8ed0c3667c9371320fa7
SergeyFierce <sergeytopskiy2003@gmail.com>
2026-03-13 12:42:42 +0300

    третий коммит

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -27,4 +27,5 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.core)
 }
--- core-ui/src/main/java/ru/topskiy/superapp/core/ui/theme/AppTheme.kt
+++ core-ui/src/main/java/ru/topskiy/superapp/core/ui/theme/AppTheme.kt
@@ -31,7 +31,6 @@ fun AppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = view.context.findActivity()?.window ?: return@SideEffect
-            val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
             window.navigationBarColor = colorScheme.background.toArgb()
 
